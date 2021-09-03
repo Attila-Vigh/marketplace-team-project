8b07b6a8d4c28558dbcdb9fd0227977006ff1281
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-09-03 14:05:51 +0100

    Done: product/Comment schema

--- src/Models/productsSchema.js
+++ src/Models/productsSchema.js
@@ -1,35 +1,35 @@
-import mongoose from 'mongoose';
-const { Schema, model } = mongoose;
+import mongoose from "mongoose"
+
+const { Schema, model } = mongoose
 
 const reviewsSchema = new Schema(
-    {
-        comment: { type: String, required: true },
-        rate: { 
-            type: Number,
-            min: [1, 'Rating must be at least 1'],
-            max: [5, 'Rating must be at most 5'], 
-            default: 5,
-        },
+  {
+    comment: { type: String, required: true },
+    rate: {
+      type: Number,
+      min: [1, "Rating must be at least 1"],
+      max: [5, "Rating must be at most 5"],
+      default: 5,
     },
-    {
-        timestamps: true,
-    }
-);
+  },
+  {
+    timestamps: true,
+  }
+)
 
-const productSchema = new Schema( 
-    {
-        name       : { type: String, required: true },
-        description: { type: String, required: true },
-        brand      : { type: String, required: true },
-        imageUrl   : { type: String, required: true },
-        category   : { type: String, required: true },
-        price      : { type: Number, required: true },
-        
-        reviews: [ { type: Schema.Types.ObjectId, ref: "Author"   }],
-    },
-    {
-        timestamps: true,
-    } 
-);
+const productSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    brand: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    category: { type: String, required: true },
+    price: { type: Number, required: true },
+    comments: { default: [], type: [reviewsSchema] },
+  },
+  {
+    timestamps: true,
+  }
+)
 
-export default model( 'Blogs', productSchema );
+export default model("Products", productSchema)
