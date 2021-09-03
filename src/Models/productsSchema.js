61c79b707d935de5ea38a6f4a4712cde7db1b1f1
Raia Kisseljova <84378854+Raissa-Kisseljova@users.noreply.github.com>
2021-09-03 15:04:31 +0100

    Reviewu basic CRUD functions added.

--- src/Models/productsSchema.js
+++ src/Models/productsSchema.js
@@ -1,20 +1,21 @@
 import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
-const reviewsSchema = new Schema(
-    {
-        comment: { type: String, required: true },
-        rate: { 
-            type: Number,
-            min: [1, 'Rating must be at least 1'],
-            max: [5, 'Rating must be at most 5'], 
-            default: 5,
-        },
-    },
-    {
-        timestamps: true,
-    }
-);
+import reviewsSchema from './reviewsSchema.js';
+// const reviewsSchema = new Schema(
+//     {
+//         comment: { type: String, required: true },
+//         rate: { 
+//             type: Number,
+//             min: [1, 'Rating must be at least 1'],
+//             max: [5, 'Rating must be at most 5'], 
+//             default: 5,
+//         },
+//     },
+//     {
+//         timestamps: true,
+//     }
+// );
 
 const productSchema = new Schema( 
     {
@@ -25,11 +26,11 @@ const productSchema = new Schema(
         category   : { type: String, required: true },
         price      : { type: Number, required: true },
         
-        reviews: [ { type: Schema.Types.ObjectId, ref: "Author"   }],
+        reviews: [ { type: Schema.Types.ObjectId, ref: "Review"   }],
     },
     {
         timestamps: true,
     } 
 );
 
-export default model( 'Blogs', productSchema );
+export default model( 'Product', productSchema );
