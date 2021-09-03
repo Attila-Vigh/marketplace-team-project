1be967f48d7a17a82ce4ec81b65298dbf509bc06
attila <attilavigh.contact@gmail.com>
2021-09-03 13:25:42 +0100

    Provisional product scheme.

--- src/Models/productsSchema.js
+++ src/Models/productsSchema.js
@@ -1,39 +1,35 @@
 import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
-const commentSchema = new Schema({
-    text: { type: String, required: true },
-    rating: { 
-        type: Number,
-        min: [1, 'Rating must be at least 1'],
-        max: [5, 'Rating must be at most 5'], 
-        default: 5,
+const reviewsSchema = new Schema(
+    {
+        comment: { type: String, required: true },
+        rate: { 
+            type: Number,
+            min: [1, 'Rating must be at least 1'],
+            max: [5, 'Rating must be at most 5'], 
+            default: 5,
+        },
     },
-    user: { 
-        name: { type: String, required: true },
-        avatar: { type: String, required: true },
-    },
-});
+    {
+        timestamps: true,
+    }
+);
 
-const blogSchema = new Schema( 
-{
-    category: { type: String, required: true },
-    title: { type: String, required: true },
-    cover: { type: String, required: true },
-    readTime: {
-        value: { type: Number, required: true },
-        unit : { type: String, required: true },
+const productSchema = new Schema( 
+    {
+        name       : { type: String, required: true },
+        description: { type: String, required: true },
+        brand      : { type: String, required: true },
+        imageUrl   : { type: String, required: true },
+        category   : { type: String, required: true },
+        price      : { type: Number, required: true },
+        
+        reviews: [ { type: Schema.Types.ObjectId, ref: "Author"   }],
     },
-    comments: { default: [], type: [ commentSchema] },
-    author: {
-        name  : { type: String, required: true },
-        email : { type: String, required: true },
-        avatar: { type: String, required: true },
-    },
-    content: { type: String, required: true },      
-},
-{
-    timestamps: true,
-} );
+    {
+        timestamps: true,
+    } 
+);
 
-export default model( 'Blogs', blogSchema );
+export default model( 'Blogs', productSchema );
--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-// import authors from "./products/router.js";
+// import products from "./products/router.js";
 // import reviews from "./reviews/router.js";
 // import cart    from "./cart/router.js";
 
 const services = Router();
 
-// services.use( "/authors", authors);
+// services.use( "/products", products);
 // services.use( "/reviews", reviews);
 // services.use( "/cart"   , cart);
 
